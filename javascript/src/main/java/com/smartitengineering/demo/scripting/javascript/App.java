35a396773e7e75ce9d24bac184248b6f3d5794e2
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-05-02 20:30:10 +0600

    Add project structure for comparing difference JVM scripting languages
    The goal of the project is to contain sample codes for parsing and running
    scripts within a file with example of implementing an interface or invoke
    an interface operation within a script.
    
    Secondary goal of the project is to compare different scripting languages'
    performance when executing similar code.
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

---
+++ javascript/src/main/java/com/smartitengineering/demo/scripting/javascript/App.java
@@ -0,0 +1,13 @@
+package com.smartitengineering.demo.scripting.javascript;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ jython/src/main/java/com/smartitengineering/demo/scripting/jython/App.java
@@ -0,0 +1,13 @@
+package com.smartitengineering.demo.scripting.jython;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
