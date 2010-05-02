70ab02d1d27aa54308b3949254d36ce5f694e3cd
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-05-02 23:20:15 +0600

    Add JRuby script sample
    A sample script getting introduced with JRuby and will build from here on.
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- jruby/src/main/java/com/smartitengineering/demo/scripting/jruby/App.java
+++ jruby/src/main/java/com/smartitengineering/demo/scripting/jruby/App.java
@@ -1,5 +1,11 @@
 package com.smartitengineering.demo.scripting.jruby;
 
+import java.io.InputStream;
+import org.jruby.embed.ScriptingContainer;
+import org.jruby.javasupport.JavaEmbedUtils;
+import org.jruby.javasupport.JavaEmbedUtils.EvalUnit;
+import org.jruby.runtime.builtin.IRubyObject;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ScriptingContainer container = new ScriptingContainer();
+        String filename = "jruby-script.rb";
+        InputStream scriptStream = App.class.getClassLoader().getResourceAsStream(filename);
+        EvalUnit evalUnit = container.parse(scriptStream, filename);
+        container.put("@test", "TEST");
+        IRubyObject rObj = evalUnit.run();
+        System.out.println(JavaEmbedUtils.rubyToJava(rObj));
     }
 }
---
+++ jruby/src/main/resources/jruby-script.rb
@@ -0,0 +1,4 @@
+def test
+  "test: "+ @test
+end
+test
